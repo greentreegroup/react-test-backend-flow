0d4ce5b29c4dddc03c4dd188bbe4d7ab0476f312
sktrinh12 <sktrinh12@gmail.com>
2024-08-29 12:34:54 -0500

    format code

--- src/urls.js
+++ src/urls.js
@@ -1,4 +1,6 @@
-export const BACKEND_UPDATE_URL = 'https://prod2-54.southeastasia.logic.azure.com:443/workflows/2b76058969d64ad4b9e178aa207020f2/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VuwzvcylF185X6zUaCzFP4v5uUNgTt9azbTm3BL9WYQ';
-export const BACKEND_DELETE_URL = (id) => `https://prod-62.southeastasia.logic.azure.com/workflows/4cd1d2e353864a52a38e36786a20f279/triggers/manual/paths/invoke/ID/${id}?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=oduyjfLAiZ63abYQXRCd4D1Iq-sR3uO5DZXMdjwGSIo`
-export const BACKEND_SHOW_DATA_URL = 'https://prod-04.southeastasia.logic.azure.com:443/workflows/e1aef793faff403fbbb9e8ccf6fb325c/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=1S7f1h9q_s7elac3oYCYTE5CsfC0IQut5lxq7hfi7rE'
-
+export const BACKEND_UPDATE_URL =
+  "https://prod2-54.southeastasia.logic.azure.com:443/workflows/2b76058969d64ad4b9e178aa207020f2/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VuwzvcylF185X6zUaCzFP4v5uUNgTt9azbTm3BL9WYQ";
+export const BACKEND_DELETE_URL = (id) =>
+  `https://prod-62.southeastasia.logic.azure.com/workflows/4cd1d2e353864a52a38e36786a20f279/triggers/manual/paths/invoke/ID/${id}?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=oduyjfLAiZ63abYQXRCd4D1Iq-sR3uO5DZXMdjwGSIo`;
+export const BACKEND_SHOW_DATA_URL =
+  "https://prod-04.southeastasia.logic.azure.com:443/workflows/e1aef793faff403fbbb9e8ccf6fb325c/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=1S7f1h9q_s7elac3oYCYTE5CsfC0IQut5lxq7hfi7rE";

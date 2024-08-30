f534c746d95681ff0c4253fc67b7221907a5db6f
sktrinh12 <sktrinh12@gmail.com>
2024-08-30 11:57:04 -0500

    format code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
- const domain = process.env.REACT_APP_AUTH0_DOMAIN;
- const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-const audience = process.env.REACT_APP_AUDIENCE
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const audience = process.env.REACT_APP_AUDIENCE;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-	<Auth0Provider
-	domain={domain}
-	clientId={clientId}
-	authorizationParams={{
-		redirect_uri: window.location.origin,
-		audience
-	}}
-	>
-	    <App />
-	</Auth0Provider>,
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      audience,
+    }}
+  >
+    <App />
+  </Auth0Provider>,
 );

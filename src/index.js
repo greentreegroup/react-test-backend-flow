a9993b01359f8236206ec31887f8faf918414240
sktrinh12 <sktrinh12@gmail.com>
2024-08-30 11:54:31 -0500

    use process.env

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import './index.css';
 import App from './App';
 import { Auth0Provider } from "@auth0/auth0-react";
 
-// const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
-const domain = 'dev-vkj8qno81b4rki7m.us.auth0.com'
-const clientId = 'ul5kQm9MFiMGVQDWDb8bVx3NflsJnpZy'
-const audience = 'https://crowdsource-automateflow.greentree.com'
+ const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+ const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const audience = process.env.REACT_APP_AUDIENCE
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
